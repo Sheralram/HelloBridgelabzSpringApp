31ba184b55fc9916f7c54996368dee1e8b3f7d55
Sheralram <ramkrishna.sheral@gmail.com>
2021-12-01 16:27:44 +0530

    Added : Welcome to Hello Bridgelabz SpringApp

---
+++ src/main/java/com/bridgelabz/hellobridgelabzspringapp/HellobridgelabzSpringappApplication.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.hellobridgelabzspringapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellobridgelabzSpringappApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Bridgelabz");
+		SpringApplication.run(HellobridgelabzSpringappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bridgelabz/hellobridgelabzspringapp/HellobridgelabzSpringappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.hellobridgelabzspringapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellobridgelabzSpringappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
